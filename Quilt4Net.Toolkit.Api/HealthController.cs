fac0c06fe39f8bd9ab092ae4c262b847fcaaf598
Daniel Bohlin <daniel.bohlin@live.se>
2025-01-10 13:06:42 +0100

    update nuget packages

--- Quilt4Net.Toolkit.Api/HealthController.cs
+++ Quilt4Net.Toolkit.Api/HealthController.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Quilt4Net.Toolkit.Api.Features.Health;
 using Quilt4Net.Toolkit.Api.Features.Live;
 using Quilt4Net.Toolkit.Api.Features.Ready;
@@ -9,7 +8,6 @@ using Microsoft.AspNetCore.Http;
 using Quilt4Net.Toolkit.Api.Features.Dependency;
 using Quilt4Net.Toolkit.Features.Health;
 using Microsoft.Extensions.Hosting;
-using System.Linq;
 
 namespace Quilt4Net.Toolkit.Api;
 
