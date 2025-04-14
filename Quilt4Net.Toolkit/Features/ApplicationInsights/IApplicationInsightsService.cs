d01644865c700d54d9b918e41e4b112260550e05
Daniel Bohlin <daniel.bohlin@live.se>
2025-04-14 17:09:02 +0200

    more details on query

--- Quilt4Net.Toolkit/Features/ApplicationInsights/IApplicationInsightsService.cs
+++ Quilt4Net.Toolkit/Features/ApplicationInsights/IApplicationInsightsService.cs
@@ -2,7 +2,7 @@
 
 public interface IApplicationInsightsService
 {
-    IAsyncEnumerable<SummaryData> GetSummaryAsync(string environment, TimeSpan timeSpan);
+    IAsyncEnumerable<SummaryData> GetSummaryAsync(string environment, TimeSpan timeSpan, SeverityLevel minSeverityLevel = SeverityLevel.Verbose);
     Task<LogDetails> GetDetails(string environment, string summaryIdentifier);
     IAsyncEnumerable<LogMeasurement> GetMeasurements(string environment, TimeSpan timeSpan);
     IAsyncEnumerable<LogItem> SearchAsync(string environment, string correlationId, TimeSpan timeSpan);
