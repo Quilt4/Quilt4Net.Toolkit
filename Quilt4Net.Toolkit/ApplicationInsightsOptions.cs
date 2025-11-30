25184f162cfbc759c6be58513599143db897bf8b
Daniel Bohlin <daniel.bohlin@live.se>
2025-11-30 23:25:30 +0100

    add .NET 9 support

--- Quilt4Net.Toolkit/ApplicationInsightsOptions.cs
+++ Quilt4Net.Toolkit/ApplicationInsightsOptions.cs
@@ -11,14 +11,15 @@ public record ApplicationInsightsOptions
     public string TenantId { get; set; }
 
     /// <summary>
-    /// This value can be dound under application insights. Click 'Workspace' under overview.
+    /// This value can be found under application insights. Click 'Workspace' under overview.
     /// </summary>
     public string WorkspaceId { get; set; }
 
     /// <summary>
-    /// Search for 'App registrations' in Azure portal or create a new App registration for Application Insights access.
-    /// When creating a new app registration, assign the API permission 'Application Insights API' with 'Data.Read' access. Also create a Client secret.
-    /// Under workspace go to 'Access control (IAM)' and add role access 'Reader' to the app registration.
+    /// Search for 'App registrations' in Azure portal or create a new App registration for Application Insights access. (Remember the app-name)
+    /// When creating a new app registration, assign the API permission 'Application Insights API' with 'Data.Read' access. (Add a permission / APIs my organization uses / Application Insights API) (Use Application, not Delegated)
+    /// Also create a Client secret that will be used for 'ClientSecret' below.
+    /// Under workspace on your application insights, go to 'Access control (IAM)' and add role access 'Reader' to the app registration with app-name.
     /// </summary>
     public string ClientId { get; set; }
 
