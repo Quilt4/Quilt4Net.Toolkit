5a191507ac12112300de0709157fb8c3374c82c2
Daniel Bohlin <daniel.bohlin@live.se>
2026-04-03 17:16:47 +0200

    fix: register IConnectionService in WASM sample
    AddQuilt4NetRemoteConfiguration registers IConnectionService which is
    required by ConnectionWrapper used in LanguageSelector/ContentAdmin.

--- Quilt4Net.Toolkit.Blazor.Wasm.Sample/Program.cs
+++ Quilt4Net.Toolkit.Blazor.Wasm.Sample/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Quilt4Net.Toolkit;
 using Quilt4Net.Toolkit.Blazor;
 using Quilt4Net.Toolkit.Blazor.Wasm.Sample;
 using Radzen;
@@ -16,5 +17,7 @@ builder.Services.AddQuilt4NetBlazorContent(builder.Configuration, o =>
     o.ApiKey = builder.Configuration["Quilt4Net:ApiKey"];
     o.Quilt4NetAddress = builder.Configuration["Quilt4Net:Quilt4NetAddress"] ?? "https://quilt4net.com/";
 });
+
+builder.Services.AddQuilt4NetRemoteConfiguration(builder.Configuration);
 
 await builder.Build().RunAsync();
